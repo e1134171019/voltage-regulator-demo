fe66b3f4c310d58bd3766b99cbbc492c0e9ba731
e1134171019 <e1134171019@gmail.com>
2026-05-28 11:56:50 +0800

    feat: integrate Scene03 rendering into SceneNodeViewer for pages 4-9 with configuration-based filtering

--- src/views/VoltageRegulatorPresentation.vue
+++ src/views/VoltageRegulatorPresentation.vue
@@ -72,6 +72,7 @@ import Slide09ExperimentSummary from '../components/presentation/slides/Slide09E
 import Slide10LaunchLinks from '../components/presentation/slides/Slide10LaunchLinks.vue'
 import BlankSlide from '../components/presentation/slides/BlankSlide.vue'
 import SceneNodeViewer from '../components/scenes/SceneNodeViewer.vue'
+import Scene03InteractiveBreadboard from '../components/scenes/Scene03InteractiveBreadboard.vue'
 
 const currentSlide = ref(0)
 const workspaceRef = ref(null)
@@ -88,7 +89,7 @@ const slideList = [
   { title: '日常生活應用', component: Slide01Opening },
   { title: '手機錄影場景', component: Slide02PhoneLoad, immersive: true },
   { title: '完整定電壓電路總覽', component: Slide07CircuitOverview },
-  { title: 'Vref：電壓參考', component: SceneNodeViewer, props: { nodeId: 'node1_vref' } },
+  { title: 'Vref：電壓參考', component: SceneNodeViewer, props: { nodeId: 'node1_vref', scene03: Scene03InteractiveBreadboard } },
   { title: 'V+：UA741 同相輸入', component: BlankSlide },
   { title: 'V−：UA741 反相輸入', component: BlankSlide },
   { title: 'UA741 OUT：修正訊號', component: BlankSlide },
